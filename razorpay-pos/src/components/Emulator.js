956dbd518623faf165a2c31333f24cd824e2951b
kirank93611 <kirank93611@gmail.com>
2024-07-04 22:29:19 +0530

    new commit

--- razorpay-pos/src/components/Emulator.js
+++ razorpay-pos/src/components/Emulator.js
@@ -1,3 +1,501 @@
+// import React, { useEffect, useState } from "react";
+// import Layout from "../assets/A50-03.png";
+
+// const Button = ({ title, onClick, style }) => {
+//   return (
+//     <button style={{ cursor: "pointer", ...style }} onClick={onClick}>
+//       {title}
+//     </button>
+//   );
+// };
+
+// const Emulator = ({ data }) => {
+//   const [screens, setScreens] = useState([]);
+//   const [index, setIndex] = useState(0);
+
+//   function handleBack() {
+//     if (index === 0) return;
+//     setIndex((index) => (index -= 1));
+//   }
+
+//   function handleNext() {
+//     if (index === screens.length - 1) return;
+//     setIndex((index) => (index += 1));
+//   }
+
+//   useEffect(() => {
+//     if (data && data.screens) setScreens(data.screens);
+//   }, [data]);
+
+//   return (
+//     <div className="emulator">
+//       <div
+//         className="android-emulator"
+//         style={{
+//           backgroundImage: `url("${Layout}")`,
+//           backgroundSize: "cover",
+//           height: "75%",
+//           display: "flex",
+//           justifyContent: "center",
+//           alignContent: "center",
+//         }}
+//       >
+//         {screens.length === 0 ? (
+//           <div
+//             style={{
+//               display: "flex",
+//               flex: 1,
+//               alignItems: "center",
+//               justifyContent: "center",
+//             }}
+//           >
+//             No screens
+//           </div>
+//         ) : (
+//           <div
+//             style={{
+//               display: "flex",
+//               flexDirection: "column",
+//               height: "75%",
+//               padding: "50px 20px",
+//             }}
+//           >
+//             <div
+//               style={{
+//                 flex: 1,
+//                 alignItems: "center",
+//               }}
+//             >
+//               {/* Title */}
+//               <h2 style={{ textAlign: "center" }}>
+//                 {screens[index]["heading-text"]}
+//               </h2>
+//               {/* Buttons */}
+//               <div
+//                 style={{
+//                   display: "flex",
+//                   flexDirection: "column",
+//                   alignItems: "center",
+//                   flex: 1,
+//                 }}
+//               >
+//                 {screens[index].widgets.map((widget, i) => {
+//                   return (
+//                     <Button
+//                       key={i}
+//                       title={widget["label-text"] ?? widget.value}
+//                     />
+//                   );
+//                 })}
+//               </div>
+//             </div>
+//             {/* Actions */}
+//             <div style={{ display: "flex", justifyContent: "space-around" }}>
+//               <Button
+//                 title={"Back"}
+//                 onClick={handleBack}
+//                 style={{
+//                   cursor: index === 0 ? "no-drop" : "pointer",
+//                   width: "120px",
+//                   padding: "8px",
+//                 }}
+//               />
+//               <Button
+//                 title={"Next"}
+//                 onClick={handleNext}
+//                 style={{
+//                   cursor: index === screens.length - 1 ? "no-drop" : "pointer",
+//                   width: "120px",
+//                   padding: "8px",
+//                 }}
+//               />
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Emulator;
+
+//code 2
+
+// import React, { useEffect, useState } from "react";
+// import Layout from "../assets/A50-03.png";
+
+// const Button = ({ title, onClick, style }) => {
+//   return (
+//     <button style={{ cursor: "pointer", ...style }} onClick={onClick}>
+//       {title}
+//     </button>
+//   );
+// };
+
+// const Emulator = ({ data }) => {
+//   const [screens, setScreens] = useState([]);
+//   const [index, setIndex] = useState(0);
+
+//   function handleBack() {
+//     if (index === 0) return;
+//     setIndex((index) => (index -= 1));
+//   }
+
+//   function handleNext() {
+//     if (index === screens.length - 1) return;
+//     setIndex((index) => (index += 1));
+//   }
+
+//   useEffect(() => {
+//     if (data && data.screens) setScreens(data.screens);
+//   }, [data]);
+
+//   return (
+//     <div className="emulator">
+//       <div
+//         className="android-emulator"
+//         style={{
+//           backgroundImage: `url("${Layout}")`,
+//         }}
+//       >
+//         {screens.length === 0 ? (
+//           <div
+//             style={{
+//               display: "flex",
+//               flex: 1,
+//               alignItems: "center",
+//               justifyContent: "center",
+//             }}
+//           >
+//             No screens
+//           </div>
+//         ) : (
+//           <div
+//             style={{
+//               display: "flex",
+//               flexDirection: "column",
+//               height: "75%",
+//               padding: "50px 20px",
+//             }}
+//           >
+//             <div
+//               style={{
+//                 flex: 1,
+//                 alignItems: "center",
+//               }}
+//             >
+//               {/* Title */}
+//               <h2 style={{ textAlign: "center" }}>
+//                 {screens[index]["heading-text"]}
+//               </h2>
+//               {/* Buttons */}
+//               <div
+//                 style={{
+//                   display: "flex",
+//                   flexDirection: "column",
+//                   alignItems: "center",
+//                   flex: 1,
+//                 }}
+//               >
+//                 {screens[index].widgets.map((widget, i) => {
+//                   return (
+//                     <Button
+//                       key={i}
+//                       title={widget["label-text"] ?? widget.value}
+//                     />
+//                   );
+//                 })}
+//               </div>
+//             </div>
+//             {/* Actions */}
+//             <div style={{ display: "flex", justifyContent: "space-around" }}>
+//               <Button
+//                 title={"Back"}
+//                 onClick={handleBack}
+//                 style={{
+//                   cursor: index === 0 ? "no-drop" : "pointer",
+//                   width: "120px",
+//                   padding: "8px",
+//                 }}
+//               />
+//               <Button
+//                 title={"Next"}
+//                 onClick={handleNext}
+//                 style={{
+//                   cursor: index === screens.length - 1 ? "no-drop" : "pointer",
+//                   width: "120px",
+//                   padding: "8px",
+//                 }}
+//               />
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Emulator;
+
+//code 3
+// import React, { useEffect, useState } from "react";
+// import Layout from "../assets/A50-03.png";
+
+// const Button = ({ title, onClick, style }) => {
+//   return (
+//     <button style={{ cursor: "pointer", ...style }} onClick={onClick}>
+//       {title}
+//     </button>
+//   );
+// };
+
+// const Emulator = ({ data }) => {
+//   const [screens, setScreens] = useState([]);
+//   const [index, setIndex] = useState(0);
+
+//   function handleBack() {
+//     if (index === 0) return;
+//     setIndex((index) => (index -= 1));
+//   }
+
+//   function handleNext() {
+//     if (index === screens.length - 1) return;
+//     setIndex((index) => (index += 1));
+//   }
+
+//   useEffect(() => {
+//     if (data && data.screens) setScreens(data.screens);
+//   }, [data]);
+
+//   return (
+//     <div className="emulator">
+//       <div
+//         className="android-emulator"
+//         style={{
+//           backgroundImage: `url("${Layout}")`,
+//           backgroundSize: "contain",
+//           backgroundRepeat: "no-repeat",
+//           width: "100%",
+//           height: "100%",
+//           position: "relative",
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         {screens.length === 0 ? (
+//           <div
+//             style={{
+//               display: "flex",
+//               flex: 1,
+//               alignItems: "center",
+//               justifyContent: "center",
+//             }}
+//           >
+//             No screens
+//           </div>
+//         ) : (
+//           <div
+//             style={{
+//               position: "absolute",
+//               top: "15%",
+//               width: "70%",
+//               height: "70%",
+//               display: "flex",
+//               flexDirection: "column",
+//               alignItems: "center",
+//               justifyContent: "space-between",
+//               padding: "20px",
+//               boxSizing: "border-box",
+//             }}
+//           >
+//             <div style={{ flex: 1, textAlign: "center" }}>
+//               <h2>{screens[index]["heading-text"]}</h2>
+//             </div>
+//             <div
+//               style={{
+//                 flex: 3,
+//                 width: "100%",
+//                 display: "flex",
+//                 flexDirection: "column",
+//                 alignItems: "center",
+//                 justifyContent: "center",
+//               }}
+//             >
+//               {screens[index].widgets.map((widget, i) => (
+//                 <Button
+//                   key={i}
+//                   title={widget["label-text"] ?? widget.value}
+//                   style={{ marginBottom: "10px" }}
+//                 />
+//               ))}
+//             </div>
+//             <div
+//               style={{
+//                 flex: 1,
+//                 display: "flex",
+//                 justifyContent: "space-around",
+//                 width: "100%",
+//               }}
+//             >
+//               <Button
+//                 title={"Back"}
+//                 onClick={handleBack}
+//                 style={{
+//                   cursor: index === 0 ? "no-drop" : "pointer",
+//                   width: "120px",
+//                   padding: "8px",
+//                 }}
+//               />
+//               <Button
+//                 title={"Next"}
+//                 onClick={handleNext}
+//                 style={{
+//                   cursor: index === screens.length - 1 ? "no-drop" : "pointer",
+//                   width: "120px",
+//                   padding: "8px",
+//                 }}
+//               />
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+//code  4
+
+// import React, { useEffect, useState } from "react";
+// import Layout from "../assets/A50-03.png";
+
+// const Button = ({ title, onClick, style }) => {
+//   return (
+//     <button style={{ cursor: "pointer", ...style }} onClick={onClick}>
+//       {title}
+//     </button>
+//   );
+// };
+
+// const Emulator = ({ data }) => {
+//   const [screens, setScreens] = useState([]);
+//   const [index, setIndex] = useState(0);
+
+//   function handleBack() {
+//     if (index === 0) return;
+//     setIndex((index) => (index -= 1));
+//   }
+
+//   function handleNext() {
+//     if (index === screens.length - 1) return;
+//     setIndex((index) => (index += 1));
+//   }
+
+//   useEffect(() => {
+//     if (data && data.screens) setScreens(data.screens);
+//   }, [data]);
+
+//   return (
+//     <div className="emulator">
+//       <div className="left-panel">
+//         <div className="background-overlay"></div>
+//       </div>
+//       <div
+//         className="android-emulator"
+//         style={{
+//           backgroundImage: `url("${Layout}")`,
+//           backgroundSize: "contain",
+//           backgroundRepeat: "no-repeat",
+//           width: "100%",
+//           height: "100%",
+//           position: "relative",
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         {screens.length === 0 ? (
+//           <div
+//             style={{
+//               display: "flex",
+//               flex: 1,
+//               alignItems: "center",
+//               justifyContent: "center",
+//             }}
+//           >
+//             No screens
+//           </div>
+//         ) : (
+//           <div
+//             style={{
+//               position: "absolute",
+//               top: "15%",
+//               width: "70%",
+//               height: "70%",
+//               display: "flex",
+//               flexDirection: "column",
+//               alignItems: "center",
+//               justifyContent: "space-between",
+//               padding: "20px",
+//               boxSizing: "border-box",
+//             }}
+//           >
+//             <div style={{ flex: 1, textAlign: "center" }}>
+//               <h2>{screens[index]["heading-text"]}</h2>
+//             </div>
+//             <div
+//               style={{
+//                 flex: 3,
+//                 width: "100%",
+//                 display: "flex",
+//                 flexDirection: "column",
+//                 alignItems: "center",
+//                 justifyContent: "center",
+//               }}
+//             >
+//               {screens[index].widgets.map((widget, i) => (
+//                 <Button
+//                   key={i}
+//                   title={widget["label-text"] ?? widget.value}
+//                   style={{ marginBottom: "10px" }}
+//                 />
+//               ))}
+//             </div>
+//             <div
+//               style={{
+//                 flex: 1,
+//                 display: "flex",
+//                 justifyContent: "space-around",
+//                 width: "100%",
+//               }}
+//             >
+//               <Button
+//                 title={"Back"}
+//                 onClick={handleBack}
+//                 style={{
+//                   cursor: index === 0 ? "no-drop" : "pointer",
+//                   width: "120px",
+//                   padding: "8px",
+//                 }}
+//               />
+//               <Button
+//                 title={"Next"}
+//                 onClick={handleNext}
+//                 style={{
+//                   cursor: index === screens.length - 1 ? "no-drop" : "pointer",
+//                   width: "120px",
+//                   padding: "8px",
+//                 }}
+//               />
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+//code 5
 import React, { useEffect, useState } from "react";
 import Layout from "../assets/A50-03.png";
 
@@ -33,11 +531,14 @@ const Emulator = ({ data }) => {
         className="android-emulator"
         style={{
           backgroundImage: `url("${Layout}")`,
-          backgroundSize: "cover",
-          height: "75%",
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          width: "100%",
+          height: "100%",
+          position: "relative",
           display: "flex",
           justifyContent: "center",
-          alignContent: "center",
+          alignItems: "center",
         }}
       >
         {screens.length === 0 ? (
@@ -54,43 +555,47 @@ const Emulator = ({ data }) => {
         ) : (
           <div
             style={{
+              position: "absolute",
+              top: "15%",
+              width: "70%",
+              height: "70%",
               display: "flex",
               flexDirection: "column",
-              height: "75%",
-              padding: "50px 20px",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "20px",
+              boxSizing: "border-box",
             }}
           >
+            <div style={{ flex: 1, textAlign: "center" }}>
+              <h2>{screens[index]["heading-text"]}</h2>
+            </div>
+            <div
+              style={{
+                flex: 3,
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {screens[index].widgets.map((widget, i) => (
+                <Button
+                  key={i}
+                  title={widget["label-text"] ?? widget.value}
+                  style={{ marginBottom: "10px" }}
+                />
+              ))}
+            </div>
             <div
               style={{
                 flex: 1,
-                alignItems: "center",
+                display: "flex",
+                justifyContent: "space-around",
+                width: "100%",
               }}
             >
-              {/* Title */}
-              <h2 style={{ textAlign: "center" }}>
-                {screens[index]["heading-text"]}
-              </h2>
-              {/* Buttons */}
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  flex: 1,
-                }}
-              >
-                {screens[index].widgets.map((widget, i) => {
-                  return (
-                    <Button
-                      key={i}
-                      title={widget["label-text"] ?? widget.value}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-            {/* Actions */}
-            <div style={{ display: "flex", justifyContent: "space-around" }}>
               <Button
                 title={"Back"}
                 onClick={handleBack}
--- razorpay-pos/src/components/LeftPanel.js
+++ razorpay-pos/src/components/LeftPanel.js
@@ -1,3 +1,140 @@
+// import React, { useState } from "react";
+// import ReactJson from "react-json-view";
+
+// const LeftPanel = ({ setData }) => {
+//   const [jsonInput, setJsonInput] = useState("");
+//   const [formattedJson, setFormattedJson] = useState("");
+
+//   const handleToggle = () => {
+//     try {
+//       const parsedJson = JSON.parse(jsonInput);
+//       const beautifiedJson = JSON.stringify(parsedJson, null, 2); // Beautify JSON with 2 spaces indentation
+//       setFormattedJson(beautifiedJson);
+//       setData(parsedJson);
+//     } catch (error) {
+//       alert("Invalid JSON");
+//     }
+//   };
+
+//   return (
+//     <div className="left-panel">
+//       <input type="text" placeholder="UserName" />
+//       <input type="password" placeholder="Password" />
+//       <button>Get Config</button>
+//       <div>
+//         <button className="toggle-button" onClick={handleToggle}>
+//           Toggle
+//         </button>
+//         <textarea
+//           value={jsonInput}
+//           onChange={(e) => setJsonInput(e.target.value)}
+//           placeholder="Enter JSON here"
+//         />
+//       </div>
+//       <select className="dropdown">
+//         <option>Get Version</option>
+//       </select>
+//       <input type="text" placeholder="Name" />
+//       <input type="text" placeholder="Description" />
+//       <input type="text" placeholder="User" />
+//       <input type="password" placeholder="Password" />
+//       <h3>Formatted JSON Output:</h3>
+//       {formattedJson === "" ? (
+//         ""
+//       ) : (
+//         <ReactJson src={JSON.parse(formattedJson)} />
+//       )}
+//     </div>
+//   );
+// };
+
+// // export default LeftPanel;
+
+// //code 2
+// // import React, { useState } from "react";
+
+// // const LeftPanel = () => {
+// //   const [formattedCode, setFormattedCode] = useState("");
+
+// //   const handleToggle = () => {
+// //     const json = {
+// //       a: "a/b/c",
+// //     };
+// //     try {
+// //       const formatted = JSON.stringify(json, null, 2).replace(/\\/g, "");
+// //       setFormattedCode(formatted);
+// //     } catch (error) {
+// //       console.error("Invalid JSON", error);
+// //     }
+// //   };
+
+// //   return (
+// //     <div className="left-panel">
+// //       <input type="text" placeholder="UserName" />
+// //       <input type="password" placeholder="Password" />
+// //       <button>Get Config</button>
+// //       <div>
+// //         <button className="toggle-button" onClick={handleToggle}>
+// //           Toggle
+// //         </button>
+// //         <textarea value={formattedCode} readOnly></textarea>
+// //       </div>
+// //       <select className="dropdown">
+// //         <option>Get Version</option>
+// //       </select>
+// //       <input type="text" placeholder="Name" />
+// //       <input type="text" placeholder="Description" />
+// //       <input type="text" placeholder="User" />
+// //       <input type="password" placeholder="Password" />
+// //     </div>
+// //   );
+// // };
+
+// // export default LeftPanel;
+
+// //code 3
+// import React, { useState } from "react";
+
+// const LeftPanel = () => {
+//   const [formattedCode, setFormattedCode] = useState("");
+
+//   const handleToggle = () => {
+//     const json = {
+//       a: "a/b/c",
+//     };
+//     try {
+//       const formatted = JSON.stringify(json, null, 2).replace(/\\/g, "");
+//       setFormattedCode(formatted);
+//     } catch (error) {
+//       console.error("Invalid JSON", error);
+//     }
+//   };
+
+//   return (
+//     <div className="left-panel">
+//       <input type="text" placeholder="UserName" />
+//       <input type="password" placeholder="Password" />
+//       <button>Get Config</button>
+//       <div>
+//         <button className="toggle-button" onClick={handleToggle}>
+//           Toggle
+//         </button>
+//         <textarea value={formattedCode} readOnly></textarea>
+//       </div>
+//       <select className="dropdown">
+//         <option>Get Version</option>
+//       </select>
+//       <input type="text" placeholder="Name" />
+//       <input type="text" placeholder="Description" />
+//       <input type="text" placeholder="User" />
+//       <input type="password" placeholder="Password" />
+//     </div>
+//   );
+// };
+
+// export default LeftPanel;
+
+//leftPanel.js;
 import React, { useState } from "react";
 import ReactJson from "react-json-view";
 
@@ -7,7 +144,7 @@ const LeftPanel = ({ setData }) => {
 
   const handleToggle = () => {
     try {
-      const parsedJson = JSON.parse(jsonInput);
+      const parsedJson = JSON.parse(jsonInput.replace(/[\\/]/g, "")); // Remove slashes and parse
       const beautifiedJson = JSON.stringify(parsedJson, null, 2); // Beautify JSON with 2 spaces indentation
       setFormattedJson(beautifiedJson);
       setData(parsedJson);
@@ -20,116 +157,34 @@ const LeftPanel = ({ setData }) => {
     <div className="left-panel">
       <input type="text" placeholder="UserName" />
       <input type="password" placeholder="Password" />
+      <div className="left-poanel">
+        <button className="toggle-button">Upload CSV</button>
+      </div>
+      <br />
       <button>Get Config</button>
       <div>
-        <button className="toggle-button" onClick={handleToggle}>
-          Toggle
-        </button>
         <textarea
           value={jsonInput}
           onChange={(e) => setJsonInput(e.target.value)}
-          placeholder="Enter JSON here"
+          placeholder="Paste JSON here"
         />
+        <br />
+        <button className="toggle-button" onClick={handleToggle}>
+          Render
+        </button>
+        <div>
+          <h1>formatted</h1>
+        </div>
+        {formattedJson && (
+          <ReactJson
+            src={JSON.parse(formattedJson)}
+            collapsed={true}
+            style={{ marginTop: "20px" }}
+          />
+        )}
       </div>
-      <select className="dropdown">
-        <option>Get Version</option>
-      </select>
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Description" />
-      <input type="text" placeholder="User" />
-      <input type="password" placeholder="Password" />
-      <h3>Formatted JSON Output:</h3>
-      {formattedJson === "" ? (
-        ""
-      ) : (
-        <ReactJson src={JSON.parse(formattedJson)} />
-      )}
     </div>
   );
 };
-
-// export default LeftPanel;
-
-//code 2
-// import React, { useState } from "react";
-
-// const LeftPanel = () => {
-//   const [formattedCode, setFormattedCode] = useState("");
-
-//   const handleToggle = () => {
-//     const json = {
-//       a: "a/b/c",
-//     };
-//     try {
-//       const formatted = JSON.stringify(json, null, 2).replace(/\\/g, "");
-//       setFormattedCode(formatted);
-//     } catch (error) {
-//       console.error("Invalid JSON", error);
-//     }
-//   };
-
-//   return (
-//     <div className="left-panel">
-//       <input type="text" placeholder="UserName" />
-//       <input type="password" placeholder="Password" />
-//       <button>Get Config</button>
-//       <div>
-//         <button className="toggle-button" onClick={handleToggle}>
-//           Toggle
-//         </button>
-//         <textarea value={formattedCode} readOnly></textarea>
-//       </div>
-//       <select className="dropdown">
-//         <option>Get Version</option>
-//       </select>
-//       <input type="text" placeholder="Name" />
-//       <input type="text" placeholder="Description" />
-//       <input type="text" placeholder="User" />
-//       <input type="password" placeholder="Password" />
-//     </div>
-//   );
-// };
-
-// export default LeftPanel;
-
-//code 3
-// import React, { useState } from "react";
-
-// const LeftPanel = () => {
-//   const [formattedCode, setFormattedCode] = useState("");
-
-//   const handleToggle = () => {
-//     const json = {
-//       a: "a/b/c",
-//     };
-//     try {
-//       const formatted = JSON.stringify(json, null, 2).replace(/\\/g, "");
-//       setFormattedCode(formatted);
-//     } catch (error) {
-//       console.error("Invalid JSON", error);
-//     }
-//   };
-
-//   return (
-//     <div className="left-panel">
-//       <input type="text" placeholder="UserName" />
-//       <input type="password" placeholder="Password" />
-//       <button>Get Config</button>
-//       <div>
-//         <button className="toggle-button" onClick={handleToggle}>
-//           Toggle
-//         </button>
-//         <textarea value={formattedCode} readOnly></textarea>
-//       </div>
-//       <select className="dropdown">
-//         <option>Get Version</option>
-//       </select>
-//       <input type="text" placeholder="Name" />
-//       <input type="text" placeholder="Description" />
-//       <input type="text" placeholder="User" />
-//       <input type="password" placeholder="Password" />
-//     </div>
-//   );
-// };
 
 export default LeftPanel;
