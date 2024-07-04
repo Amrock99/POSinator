aff4d11d8bf7cf03839d39873500718adb0f6c8a
kirank93611 <kirank93611@gmail.com>
2024-07-04 23:31:19 +0530

    1131pm fina

--- razorpay-pos/src/components/LeftPanel.js
+++ razorpay-pos/src/components/LeftPanel.js
@@ -1,144 +1,8 @@
-// import React, { useState } from "react";
-// import ReactJson from "react-json-view";
-
-// const LeftPanel = ({ setData }) => {
-//   const [jsonInput, setJsonInput] = useState("");
-//   const [formattedJson, setFormattedJson] = useState("");
-
-//   const handleToggle = () => {
-//     try {
-//       const parsedJson = JSON.parse(jsonInput);
-//       const beautifiedJson = JSON.stringify(parsedJson, null, 2); // Beautify JSON with 2 spaces indentation
-//       setFormattedJson(beautifiedJson);
-//       setData(parsedJson);
-//     } catch (error) {
-//       alert("Invalid JSON");
-//     }
-//   };
-
-//   return (
-//     <div className="left-panel">
-//       <input type="text" placeholder="UserName" />
-//       <input type="password" placeholder="Password" />
-//       <button>Get Config</button>
-//       <div>
-//         <button className="toggle-button" onClick={handleToggle}>
-//           Toggle
-//         </button>
-//         <textarea
-//           value={jsonInput}
-//           onChange={(e) => setJsonInput(e.target.value)}
-//           placeholder="Enter JSON here"
-//         />
-//       </div>
-//       <select className="dropdown">
-//         <option>Get Version</option>
-//       </select>
-//       <input type="text" placeholder="Name" />
-//       <input type="text" placeholder="Description" />
-//       <input type="text" placeholder="User" />
-//       <input type="password" placeholder="Password" />
-//       <h3>Formatted JSON Output:</h3>
-//       {formattedJson === "" ? (
-//         ""
-//       ) : (
-//         <ReactJson src={JSON.parse(formattedJson)} />
-//       )}
-//     </div>
-//   );
-// };
-
-// // export default LeftPanel;
-
-// //code 2
-// // import React, { useState } from "react";
-
-// // const LeftPanel = () => {
-// //   const [formattedCode, setFormattedCode] = useState("");
-
-// //   const handleToggle = () => {
-// //     const json = {
-// //       a: "a/b/c",
-// //     };
-// //     try {
-// //       const formatted = JSON.stringify(json, null, 2).replace(/\\/g, "");
-// //       setFormattedCode(formatted);
-// //     } catch (error) {
-// //       console.error("Invalid JSON", error);
-// //     }
-// //   };
-
-// //   return (
-// //     <div className="left-panel">
-// //       <input type="text" placeholder="UserName" />
-// //       <input type="password" placeholder="Password" />
-// //       <button>Get Config</button>
-// //       <div>
-// //         <button className="toggle-button" onClick={handleToggle}>
-// //           Toggle
-// //         </button>
-// //         <textarea value={formattedCode} readOnly></textarea>
-// //       </div>
-// //       <select className="dropdown">
-// //         <option>Get Version</option>
-// //       </select>
-// //       <input type="text" placeholder="Name" />
-// //       <input type="text" placeholder="Description" />
-// //       <input type="text" placeholder="User" />
-// //       <input type="password" placeholder="Password" />
-// //     </div>
-// //   );
-// // };
-
-// // export default LeftPanel;
-
-// //code 3
-// import React, { useState } from "react";
-
-// const LeftPanel = () => {
-//   const [formattedCode, setFormattedCode] = useState("");
-
-//   const handleToggle = () => {
-//     const json = {
-//       a: "a/b/c",
-//     };
-//     try {
-//       const formatted = JSON.stringify(json, null, 2).replace(/\\/g, "");
-//       setFormattedCode(formatted);
-//     } catch (error) {
-//       console.error("Invalid JSON", error);
-//     }
-//   };
-
-//   return (
-//     <div className="left-panel">
-//       <input type="text" placeholder="UserName" />
-//       <input type="password" placeholder="Password" />
-//       <button>Get Config</button>
-//       <div>
-//         <button className="toggle-button" onClick={handleToggle}>
-//           Toggle
-//         </button>
-//         <textarea value={formattedCode} readOnly></textarea>
-//       </div>
-//       <select className="dropdown">
-//         <option>Get Version</option>
-//       </select>
-//       <input type="text" placeholder="Name" />
-//       <input type="text" placeholder="Description" />
-//       <input type="text" placeholder="User" />
-//       <input type="password" placeholder="Password" />
-//     </div>
-//   );
-// };
-
-// export default LeftPanel;
-
-//leftPanel.js;
+//code
 import React, { useState } from "react";
 import ReactJson from "react-json-view";
 
-const LeftPanel = ({ setData }) => {
+const LeftPanel = ({ setData, onUploadCSV, onGetConfig }) => {
   const [jsonInput, setJsonInput] = useState("");
   const [formattedJson, setFormattedJson] = useState("");
 
@@ -157,9 +21,7 @@ const LeftPanel = ({ setData }) => {
     <div className="left-panel">
       <input type="text" placeholder="UserName" />
       <input type="password" placeholder="Password" />
-      <div className="left-poanel">
-        <button className="toggle-button">Upload CSV</button>
-      </div>
+      <button>Upload CSV</button>
       <br />
       <button>Get Config</button>
       <div>
@@ -168,13 +30,15 @@ const LeftPanel = ({ setData }) => {
           onChange={(e) => setJsonInput(e.target.value)}
           placeholder="Paste JSON here"
         />
-        <br />
         <button className="toggle-button" onClick={handleToggle}>
           Render
         </button>
-        <div>
-          <h1>formatted</h1>
-        </div>
+        <br />
+
+        <label>
+          <h1>Formatted JSON</h1>
+        </label>
+
         {formattedJson && (
           <ReactJson
             src={JSON.parse(formattedJson)}
